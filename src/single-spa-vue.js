10aa166f6a43c3c8618ffbc06326acbcf0addf44
Joel Denning <joel.denning@canopytax.com>
2018-01-20 00:04:24 -0800

    Tabs -> spaces

--- src/single-spa-vue.js
+++ src/single-spa-vue.js
@@ -1,57 +1,57 @@
 const defaultOpts = {
-	// required opts
-	Vue: null,
-	appOptions: null,
-	template: null,
+  // required opts
+  Vue: null,
+  appOptions: null,
+  template: null,
 }
 
 export default function singleSpaReact(userOpts) {
-	if (typeof userOpts !== 'object') {
-		throw new Error(`single-spa-vue requires a configuration object`);
-	}
+  if (typeof userOpts !== 'object') {
+    throw new Error(`single-spa-vue requires a configuration object`);
+  }
 
-	const opts = {
-		...defaultOpts,
-		...userOpts,
-	};
+  const opts = {
+    ...defaultOpts,
+    ...userOpts,
+  };
 
-	if (!opts.Vue) {
-		throw new Error('single-spa-vuejs must be passed opts.Vue');
-	}
+  if (!opts.Vue) {
+    throw new Error('single-spa-vuejs must be passed opts.Vue');
+  }
 
-	if (!opts.appOptions) {
-		throw new Error('single-spa-vuejs must be passed opts.appOptions');
-	}
+  if (!opts.appOptions) {
+    throw new Error('single-spa-vuejs must be passed opts.appOptions');
+  }
 
-	// Just a shared object to store the mounted object state
-	let mountedInstances = {};
+  // Just a shared object to store the mounted object state
+  let mountedInstances = {};
 
-	return {
-		bootstrap: bootstrap.bind(null, opts, mountedInstances),
-		mount: mount.bind(null, opts, mountedInstances),
-		unmount: unmount.bind(null, opts, mountedInstances),
-	};
+  return {
+    bootstrap: bootstrap.bind(null, opts, mountedInstances),
+    mount: mount.bind(null, opts, mountedInstances),
+    unmount: unmount.bind(null, opts, mountedInstances),
+  };
 }
 
 function bootstrap(opts) {
-	return Promise.resolve();
+  return Promise.resolve();
 }
 
 function mount(opts, mountedInstances) {
-	return new Promise((resolve, reject) => {
-		mountedInstances.instance = new opts.Vue(opts.appOptions);
+  return new Promise((resolve, reject) => {
+    mountedInstances.instance = new opts.Vue(opts.appOptions);
     if (mountedInstances.instance.bind) {
       mountedInstances.instance = mountedInstances.instance.bind(mountedInstances.instance);
     }
-		resolve();
-	});
+    resolve();
+  });
 }
 
 function unmount(opts, mountedInstances) {
-	return new Promise((resolve, reject) => {
-		mountedInstances.instance.$destroy();
-		mountedInstances.instance.$el.innerHTML = '';
-		delete mountedInstances.instance;
-		resolve();
-	});
+  return new Promise((resolve, reject) => {
+    mountedInstances.instance.$destroy();
+    mountedInstances.instance.$el.innerHTML = '';
+    delete mountedInstances.instance;
+    resolve();
+  });
 }
