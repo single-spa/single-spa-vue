7650ca4e3b16cacfd9813f5cef2488d77dda57d1
Joel Denning <joeldenning@gmail.com>
2020-01-10 13:49:41 -0700

    Using CSS.escape instead of manual (incomplete) escaping of selector (#29)
    * Using CSS.escape instead of manual (incomplete) escaping of selector
    
    * Anthony's feedback

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import babel from 'rollup-plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+  input: './src/single-spa-vue.js',
+  output: {
+    dir: 'lib',
+    name: 'singleSpaVue',
+    sourcemap: true,
+    format: 'umd'
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve(),
+    commonjs(),
+    terser()
+  ]
+}
