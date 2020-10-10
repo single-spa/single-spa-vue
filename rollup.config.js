343cdc7a61a0dc82192cd74356bde65b035e5455
Marco A. Bomfim <marco.bomfim@zenvia.com>
2020-10-09 21:43:30 -0300

    chore: include parcel into rollup compilation

--- rollup.config.js
+++ rollup.config.js
@@ -2,21 +2,42 @@ import babel from "rollup-plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
+import vuePlugin from "rollup-plugin-vue";
 
-export default {
-  input: "./src/single-spa-vue.js",
-  output: {
-    dir: "lib",
-    name: "singleSpaVue",
-    sourcemap: true,
-    format: "umd"
+export default [
+  {
+    input: "./src/single-spa-vue.js",
+    output: {
+      dir: "lib",
+      name: "singleSpaVue",
+      sourcemap: true,
+      format: "umd"
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      resolve(),
+      commonjs(),
+      terser()
+    ]
   },
-  plugins: [
-    babel({
-      exclude: "node_modules/**"
-    }),
-    resolve(),
-    commonjs(),
-    terser()
-  ]
-};
+  {
+    input: "./src/parcel.vue",
+    output: {
+      dir: "lib",
+      name: "parcel",
+      sourcemap: true,
+      format: "umd"
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      resolve(),
+      commonjs(),
+      terser(),
+      vuePlugin()
+    ]
+  }
+];
