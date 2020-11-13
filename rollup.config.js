6f974dde2f5be272e58589ed4a27172a0bd4d775
Marco Aurélio Bomfim <marcoabomfim3@gmail.com>
2020-11-13 19:17:57 -0300

    Add parcel component for single-spa-vue (#53)
    * configure package.json to parse .vue file tests
    
    * add simple parcel component and initial tests
    
    * remove moduleNameWrapper from package.json
    
    * switch to render function instead of template
    
    * allow mounting to specified element from config
    
    * refactor(parcel): switch to appending element to refs, instead of vDOM
    
    * chore(parcel): add default appendTo element, and include posibility of adding style to parcel
    
    * chore(parcel): adds the addThingToDo method, to be called before the parcel's lifecycle
    
    * chore: include parcel into rollup compilation
    
    * fix: fix addThingToDo parameter insertion
    
    * test: fix tests that were not running as async
    
    * Finish implementation of parcel component.
    
    * Remove unnecessary deps
    
    * Adding test for passing in promise for config object.
    
    Co-authored-by: Marco A. Bomfim <marco.bomfim@zenvia.com>
    Co-authored-by: Joel Denning <joeldenning@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -4,27 +4,47 @@ import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 
 export default [
-  createConfig("umd"),
-  createConfig("esm"),
-  createConfig("system"),
+  ...createConfig("umd"),
+  ...createConfig("esm"),
+  ...createConfig("system"),
 ];
 
 function createConfig(format) {
-  return {
-    input: "./src/single-spa-vue.js",
-    output: {
-      dir: `dist/${format}`,
-      name: format === "umd" ? "singleSpaVue" : null,
-      sourcemap: true,
-      format: format,
+  return [
+    {
+      input: "./src/single-spa-vue.js",
+      output: {
+        dir: `dist/${format}`,
+        name: format === "umd" ? "singleSpaVue" : null,
+        sourcemap: true,
+        format: format,
+      },
+      plugins: [
+        babel({
+          exclude: "node_modules/**",
+        }),
+        resolve(),
+        commonjs(),
+        terser(),
+      ],
     },
-    plugins: [
-      babel({
-        exclude: "node_modules/**",
-      }),
-      resolve(),
-      commonjs(),
-      terser(),
-    ],
-  };
+    {
+      input: "./src/parcel.js",
+      output: {
+        dir: `dist/${format}`,
+        name: format === "umd" ? "singleSpaVueParcel" : null,
+        sourcemap: true,
+        format,
+      },
+      plugins: [
+        babel({
+          exclude: "node_modules/**",
+        }),
+        resolve(),
+        commonjs(),
+        terser(),
+      ],
+      external: ["vue"],
+    },
+  ];
 }
---
+++ src/parcel.test.js
@@ -0,0 +1,252 @@
+import { mount } from "@vue/test-utils";
+import { mountRootParcel } from "single-spa";
+import Parcel from "./parcel.js";
+
+describe("Parcel", () => {
+  let wrapper;
+
+  afterEach(() => {
+    if (wrapper) {
+      wrapper.destroy();
+    }
+
+    wrapper = null;
+  });
+
+  it("should throw an error if incorrect props are provided", async () => {
+    try {
+      mount(Parcel);
+      fail("Mounting should fail without config prop");
+    } catch (err) {
+      expect(err.message).toMatch(/component requires a config prop/);
+    }
+  });
+
+  it("should render if config and mountParcel are provided", async () => {
+    const wrapper = await mount(Parcel, {
+      propsData: {
+        config: createParcelConfig(),
+        mountParcel: mountRootParcel,
+      },
+    });
+
+    await tick();
+
+    expect(wrapper.emitted().parcelMounted).toBeTruthy();
+
+    expect(wrapper.find("button#parcel").exists()).toBe(true);
+    expect(wrapper.find("button#parcel").text()).toEqual("The parcel button");
+  });
+
+  it("should wrap with to div if no 'wrapWith' is provided", async () => {
+    const wrapper = await mount(Parcel, {
+      propsData: {
+        config: createParcelConfig(),
+        mountParcel: mountRootParcel,
+      },
+    });
+
+    await tick();
+
+    expect(wrapper.emitted().parcelMounted).toBeTruthy();
+
+    expect(wrapper.find("div").exists()).toBe(true);
+  });
+
+  it("should respect the wrapWith, wrapClass, and wrapStyle props", async () => {
+    wrapper = await mount(Parcel, {
+      propsData: {
+        config: createParcelConfig(),
+        wrapWith: "span",
+        wrapClass: "the-class",
+        wrapStyle: {
+          backgroundColor: "red",
+        },
+        mountParcel: mountRootParcel,
+      },
+    });
+
+    await tick();
+
+    expect(wrapper.emitted().parcelMounted).toBeTruthy();
+
+    expect(wrapper.find("span").exists()).toBe(true);
+    expect(wrapper.find("span").classes()).toContain("the-class");
+    expect(wrapper.find("span").attributes("style")).toEqual(
+      "background-color: red;"
+    );
+
+    expect(wrapper.find("span").find("button#parcel").exists()).toBe(true);
+    expect(wrapper.find("span").find("button#parcel").text()).toBe(
+      "The parcel button"
+    );
+  });
+
+  it("should unmount properly", async () => {
+    const config = createParcelConfig();
+    const wrapper = await mount(Parcel, {
+      propsData: {
+        config,
+        mountParcel: mountRootParcel,
+      },
+    });
+
+    await tick();
+
+    expect(wrapper.emitted().parcelMounted).toBeTruthy();
+    expect(config.mounted).toBe(true);
+
+    expect(wrapper.find("button#parcel").exists()).toBe(true);
+
+    await wrapper.destroy();
+
+    await tick();
+
+    expect(config.mounted).toBe(false);
+  });
+
+  it("forwards parcelProps to the parcel", async () => {
+    const config = createParcelConfig();
+    const wrapper = await mount(Parcel, {
+      propsData: {
+        config,
+        mountParcel: mountRootParcel,
+        parcelProps: {
+          foo: "bar",
+        },
+      },
+    });
+
+    await tick();
+
+    expect(wrapper.emitted().parcelMounted).toBeTruthy();
+    expect(config.mounted).toBe(true);
+    expect(config.props).toMatchObject({ foo: "bar" });
+  });
+
+  it("calls parcel.update when update is defined", async () => {
+    const config = createParcelConfig({ update: true });
+
+    const wrapper = await mount(Parcel, {
+      propsData: {
+        config,
+        mountParcel: mountRootParcel,
+        parcelProps: {
+          numUsers: 10,
+        },
+      },
+    });
+
+    await tick();
+
+    expect(wrapper.emitted().parcelMounted).toBeTruthy();
+    expect(config.mounted).toBe(true);
+    expect(config.props).toMatchObject({
+      numUsers: 10,
+    });
+
+    wrapper.setProps({
+      parcelProps: {
+        numUsers: 100,
+      },
+    });
+
+    await tick();
+
+    expect(wrapper.emitted().parcelUpdated).toBeTruthy();
+    expect(config.props).toMatchObject({
+      numUsers: 100,
+    });
+  });
+
+  it(`doesn't die when the parcel config doesn't have an update function`, async () => {
+    const config = createParcelConfig();
+
+    const wrapper = await mount(Parcel, {
+      propsData: {
+        config,
+        mountParcel: mountRootParcel,
+        parcelProps: {
+          numUsers: 10,
+        },
+      },
+    });
+
+    await tick();
+
+    expect(wrapper.emitted().parcelMounted).toBeTruthy();
+    expect(config.mounted).toBe(true);
+    expect(config.props).toMatchObject({
+      numUsers: 10,
+    });
+
+    wrapper.setProps({
+      parcelProps: {
+        numUsers: 100,
+      },
+    });
+
+    await tick();
+
+    expect(wrapper.emitted().parcelUpdated).toBeFalsy();
+    expect(config.props).toMatchObject({
+      // since the parcel config doesn't have an update function,
+      // the numUsers prop on the parcel won't update when the
+      // <parcel> vue component updates
+      numUsers: 10,
+    });
+  });
+
+  it(`allows you to pass in a promise that resolves with the config object`, async () => {
+    const config = createParcelConfig();
+
+    const wrapper = await mount(Parcel, {
+      propsData: {
+        config: Promise.resolve(config),
+        mountParcel: mountRootParcel,
+      },
+    });
+
+    await tick();
+
+    expect(wrapper.emitted().parcelMounted).toBeTruthy();
+    expect(config.mounted).toBe(true);
+    expect(wrapper.find("button#parcel").exists()).toBe(true);
+  });
+});
+
+function createParcelConfig(opts = {}) {
+  const result = {
+    async mount(props) {
+      const button = document.createElement("button");
+      button.textContent = `The parcel button`;
+      button.id = "parcel";
+      props.domElement.appendChild(button);
+      result.mounted = true;
+      result.props = props;
+    },
+    async unmount(props) {
+      props.domElement.querySelector("button").remove();
+      result.mounted = false;
+      result.props = props;
+    },
+    mounted: false,
+    props: null,
+    numUpdates: 0,
+  };
+
+  if (opts.update) {
+    result.update = async (props) => {
+      result.props = props;
+      result.numUpdates++;
+    };
+  }
+
+  return result;
+}
+
+function tick() {
+  return new Promise((resolve) => {
+    setTimeout(resolve);
+  });
+}
