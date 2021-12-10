d615f450cbdf299e6695eb58552674b663892a72
王学波 <wangxuebo@datafountain.cn>
2021-12-10 15:08:23 +0800

    fix: merge appOptions.data and mount props error

--- src/single-spa-vue.js
+++ src/single-spa-vue.js
@@ -127,7 +127,11 @@ function mount(opts, mountedInstances, props) {
         appOptions.data = {};
       }
 
-      appOptions.data = () => ({ ...appOptions.data, ...props });
+      const originData = appOptions.data;
+      appOptions.data = function() {
+        const data = typeof originData === "function" ? originData.call(this, this) : originData;
+        return {...data, ...props};
+      };
 
       if (opts.createApp) {
         instance.vueInstance = opts.createApp(appOptions);
