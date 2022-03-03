4f26a4cfbd1588937d4bd308417cf39a85be825a
Tom Wilkins <tomw@aferry.com>
2022-03-03 15:15:23 +0000

    Linting

--- src/parcel.js
+++ src/parcel.js
@@ -38,7 +38,7 @@ export default {
 
       this.nextThingToDo = (this.nextThingToDo || Promise.resolve())
         .then((...args) => {
-          if (this.unmounted && action !== 'unmount') {
+          if (this.unmounted && action !== "unmount") {
             return;
           }
 
@@ -65,7 +65,7 @@ export default {
       });
     },
     singleSpaUnmount() {
-      if (this.parcel && this.parcel.getStatus() === 'MOUNTED') {
+      if (this.parcel && this.parcel.getStatus() === "MOUNTED") {
         return this.parcel.unmount();
       }
     },
