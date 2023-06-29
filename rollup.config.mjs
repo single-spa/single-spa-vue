eda60240f686f1ed5e6fdd3148326a605d74d78f
joeldenning <joeldenning@gmail.com>
2023-06-29 14:07:54 -0600

    Upgrade dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import { babel } from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 export default [
   ...createConfig("umd"),
