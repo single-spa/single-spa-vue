2ada712936031e403adae66956f3862922f72e2b
Jolyn Denning <jolyndenning@gmail.com>
2025-05-19 12:34:38 -0600

    rewrite

--- src/single-spa-vue.ts
+++ src/single-spa-vue.ts
@@ -1,80 +1,53 @@
-import { AppProps, LifeCycles } from "single-spa";
-import { App, Component, createApp, createSSRApp } from "vue";
-import type { Component as Vue2Component } from "vue2";
+import type { AppProps, LifeCycles } from "single-spa";
+import type { App, Component, createApp, createSSRApp } from "vue";
 import {
   chooseDomElementGetter,
   DomElementGetterOpts,
 } from "dom-element-getter-helpers";
 
-type SingleSpaVueOptions<ExtraProps> =
-  | SingleSpaVue2Options<ExtraProps>
-  | SingleSpaVue3Options<ExtraProps>;
-
-interface SingleSpaVue3Options<ExtraProps> extends DomElementGetterOpts {
+interface SingleSpaVueOptions<ExtraProps> extends DomElementGetterOpts {
   rootComponent:
     | Component<ExtraProps>
     | ((props: AppProps & ExtraProps) => Promise<Component<ExtraProps>>);
   createApp: typeof createApp | typeof createSSRApp;
-  customizeInstance?(app: App): void;
-}
-
-interface SingleSpaVue2Options<ExtraProps> extends DomElementGetterOpts {
-  Vue: any;
-  rootComponent:
-    | Vue2Component<ExtraProps>
-    | ((props: AppProps & ExtraProps) => Promise<Vue2Component<ExtraProps>>);
+  setupInstance?(app: App): void;
 }
 
 export default function singleSpaVue<ExtraProps>(
   opts: SingleSpaVueOptions<ExtraProps>,
 ): LifeCycles<ExtraProps> {
-  if (opts["createApp"]) {
-    const vue3Opts = opts as SingleSpaVue3Options<ExtraProps>;
-    const mountedInstances: Record<string, App> = {};
-    let RootComponent: Component<AppProps & ExtraProps>;
+  const mountedInstances: Record<string, App> = {};
+  let RootComponent: Component<AppProps & ExtraProps>;
 
-    return {
-      async bootstrap(props) {
-        if (typeof vue3Opts.rootComponent === "function") {
-          RootComponent = await (vue3Opts.rootComponent as Function)(props);
-        } else {
-          RootComponent = vue3Opts.rootComponent as Component;
-        }
+  return {
+    async init(props) {
+      if (typeof opts.rootComponent === "function") {
+        RootComponent = await (opts.rootComponent as Function)(props);
+      } else {
+        RootComponent = opts.rootComponent as Component;
+      }
 
-        // @ts-ignore
-        // https://vuejs.org/api/options-misc.html#inheritattrs
-        // The single-spa props should not be inherited as DOM attributes
-        RootComponent.inheritAttrs = false;
-      },
-      async mount(props) {
-        const app = createApp(RootComponent, props);
-        if (vue3Opts["customizeInstance"]) {
-          vue3Opts.customizeInstance(app);
-        }
-        const domElement = chooseDomElementGetter(vue3Opts, props)();
-        app.mount(domElement);
-        mountedInstances[props.name] = app;
-      },
-      async update(props) {},
-      async unmount(props) {
-        const app = mountedInstances[props.name];
-        return app!.unmount();
-      },
-    };
-  } else {
-    let RootComponent: Vue2Component;
-
-    return {
-      async bootstrap(props) {
-        if (typeof opts.rootComponent === "function") {
-          RootComponent = await (opts.rootComponent as Function)(props);
-        } else {
-          RootComponent = opts.rootComponent as Vue2Component;
-        }
-      },
-      async mount(props) {},
-      async update(props) {},
-      async unmount(props) {},
-    };
-  }
+      // https://vuejs.org/api/options-misc.html#inheritattrs
+      // The single-spa props should not be inherited as DOM attributes
+      // @ts-ignore
+      RootComponent.inheritAttrs = false;
+    },
+    async mount(props) {
+      const app = opts.createApp(RootComponent, props);
+      if (opts["setupInstance"]) {
+        opts.setupInstance(app);
+      }
+      const domElement = chooseDomElementGetter(opts, props)();
+      app.mount(domElement);
+      mountedInstances[props.name] = app;
+    },
+    async update(props) {},
+    async unmount(props) {
+      const app = mountedInstances[props.name];
+      app!.unmount();
+      const domElement = chooseDomElementGetter(opts, props)();
+      domElement.remove();
+      delete mountedInstances[props.name];
+    },
+  };
 }
